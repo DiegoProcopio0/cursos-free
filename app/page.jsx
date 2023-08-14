328b4b6fd47a556d0928efca56c955940cb289a3
Diego <diegprocopio98@gmail.com>
2023-08-14 17:28:30 -0300

    revome Slider.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,13 @@
 import Header from "./components/Header";
-import SliderTop from "./components/Slider";
 
 export default function Home() {
  return (
     <div>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <SliderTop />
         <h1
           className="font-bold text-purple-800 text-lg uppercase hover:text-black"
-        >Bianca feia</h1>
-
+        >Cursos free</h1>
       </main>
     </div>
   )
