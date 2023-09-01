b7ab9e1144a07a4250239b5be30667db174f8e1c
Diego <diegprocopio98@gmail.com>
2023-09-01 16:28:29 -0300

    add cards

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,31 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Card() {
+export default function Card({link,  image, course, school, description}) {
+
+
     return (
         <div className={`
-             min-h-min mb-4 flex flex-col items-center justify-center py-2 px-3 bg-slate-300 w-80 rounded-md hover:scale-110 transition-all ease-in-out
+            min-h-[400px] max-h-[400px] h-auto  mb-4 flex flex-col items-center justify-start py-2 px-3 bg-slate-300 w-80 rounded-md hover:scale-110 transition-all ease-in-out
         `}>
-            <Link href="https://www.google.com/">
+            <Link href={link}>
                 <Image 
-                    src="/assets/images/next.webp"
-                    alt="sa"
+                    className="w-auto max-h-[180px] min-w-full object-cover "
+                    src={image}
                     width={300}
-                    height={300}    
-                    />
+                    height={300}
+                    alt="sa" 
+                />
                 <div
                     className={`flex flex-col text-black justify-center w-full gap-1 py-2`}
                 >
                     <h2
                         className={`text-black font-bold mx-auto`}
-                    >Curso de JS</h2>
+                    >{course}</h2>
                     <h3
                         className={`text-gray-500 italic text-sm`}
-                        >Diego Procopio</h3>
+                        >{school}</h3>
                     <span
                         className={`text-gray-800 font-medium mx-auto`}
                         >
-                        Descrição do Curso de JS do Diego
+                        {description}
                     </span>
                 </div>
             </Link>
--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -1,6 +1,18 @@
 import  Card  from "./Card";
+import NextJs from "../../public/assets/images/next.webp" 
+import Html from "../../public/assets/images/html.png" 
+import Java from "../../public/assets/images/java.png" 
 
 export default function Cards() {
+
+    // const cards = {
+    //     course:"HTML – Avançado",
+    //     link: "https://www.ev.org.br/cursos/html-avancado",
+    //     image: {Html},
+    //     school: "Instituição Bradesco",
+    //     description: "Dividido em quatro módulos, este curso traz conceitos que complementam o conteúdo apresentado em nosso curso de HTML – Básico." 
+    // }
+
     return (
         <div>
             <h1
@@ -11,11 +23,20 @@ export default function Cards() {
             <section id="cards"
                 className={`flex flex-wrap w-full justify-center items-center mb-8 mt-4 gap-3`}
                 >
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                <Card
+                    course="HTML – Avançado"
+                    link="https://www.ev.org.br/cursos/html-avancado"
+                    image={Html}
+                    school="Instituição Bradesco"
+                    description="Dividido em quatro módulos, este curso traz conceitos que complementam o conteúdo apresentado em nosso curso de HTML – Básico. "
+                />
+                <Card
+                    course="Java"
+                    link="https://www.ev.org.br/cursos/linguagem-de-programacao-java-avancado"
+                    image={Java}
+                    school="Instituição Bradesco"
+                    description="Fundamentos de (POO) serão evidenciados, passando pelos objetos, as classes, suas construções, além dos complementos da linguagem Java, incluindo conceitos, métodos e herança"
+                />
             </section>
         </div>
     )
